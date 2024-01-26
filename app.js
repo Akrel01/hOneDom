cbf59516df2eb9aaf08ee6336fe02956fa813797
Alexander Tejeda <julioalexandertejeda.p.2001@gmail.com>
2024-01-26 18:12:34 -0500

    two

--- app.js
+++ app.js
@@ -49,28 +49,3 @@ for (let index = 0; index < array.length; index++) {
 
 
 
-
-// let nombres = Object.keys(array).map(key =>{
-//     const value = array[key]
-// } )
-
-  
-//  persona = array[0].nombre + " " + array[0].apellido;
-
-// const lista = document.getElementById("lista"); //Referencia donde colocare el elemento creado
-
-// const listadoPersonas = document.createElement("li"); // Crear el elemento
-
-// lista.append(listadoPersonas); //Colocar el elemento creado.
-
-// listadoPersonas.innerText = Object.values(array[0]); //Escribo la persona en el dom.
-
-// // let hola = Object.values(array[0]); obtener los valores del objeto en el indice del array.
-// // console.log(hola)
-
-
-    
-   
-//      console.log(personas);
-
-
